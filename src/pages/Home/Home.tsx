02ae72149e461f04cdafba35a500a4488c75cc0a
AugustoCVS <guty.vertolis@gmail.com>
2023-07-04 08:46:29 -0300

    feat: start creating timer forms

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import {
   CountDownContainer,
   FormContainer,
@@ -10,15 +11,25 @@ import {
 } from './styles'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="taskSuggestions"
             placeholder="De um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="taskSuggestions">
@@ -35,6 +46,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -48,7 +60,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCounDownButton type="submit">
+        <StartCounDownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCounDownButton>
