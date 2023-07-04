dde3b86235c4398eb82d7169227577240a400439
AugustoCVS <guty.vertolis@gmail.com>
2023-07-03 23:49:17 -0300

    feat: creating home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,45 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCounDownButton,
+  TaskInput,
+} from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="De um nome para o seu projeto" />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minuteAmount"
+            placeholder="00"
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCounDownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCounDownButton>
+      </form>
+    </HomeContainer>
   )
 }
